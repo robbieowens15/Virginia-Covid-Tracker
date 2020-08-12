15c1647957ed3b4d15d2b52d579673f5760b935b
Robert Owens <raowens15@icloud.com>
2020-08-12 15:36:46 -0400

    Unsubscribe and Resubscribe Works

--- flask_email_signup/routes.py
+++ flask_email_signup/routes.py
@@ -12,7 +12,7 @@ def subscribe():
         recipient = modles.Recipient(email = form.email.data, locality=form.location.data)
         db.session.add(recipient)
         db.session.commit()
-        flash(f'{form.email.data} has been signed up!', 'success')
+        flash(f'{form.email.data} has been signed up for {form.location.data}!', 'success')
         return redirect(url_for('subscribe'))
     return render_template('subscribe.html', title='Subsribe',form=form)
 
@@ -20,6 +20,9 @@ def subscribe():
 def unsubscribe():
     form = UnsubscribeForm()
     if form.validate_on_submit():
+        recipient_to_delete = modles.Recipient.query.filter_by(email=form.email.data).first()
+        db.session.delete(recipient_to_delete)
+        db.session.commit()
         flash(f'{form.email.data} will stop recieving emails', 'success')
         return redirect(url_for('unsubscribe'))
     return render_template('unsubscribe.html', title='Unsubscribe', form=form)
