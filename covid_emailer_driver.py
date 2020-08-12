e078ad4cb8e6801361bd106b5741f2e53503a19e
Robert Owens <raowens15@icloud.com>
2020-08-12 18:21:47 -0400

    Make Sure Emails Only Deleivered at cetrain time

--- covid_emailer_driver.py
+++ covid_emailer_driver.py
@@ -1,10 +1,13 @@
 import os
-
+import datetime
+import time
 import csv_obtainer as csv
 import process_data as pd
 import html_generator as html
 import email_sender as es
 
+LOWER_TIME_LIMIT = datetime.datetime(hour=18,minute=19,second=0,microsecond=0)
+UPPER_TIME_LIMIT = datetime.datetime(hour=18,minute=20,second=0,microsecond=0)
 def main():
     csv.dowload_csv()
     list_of_localities = pd.get_locality_list_from_db()
@@ -38,5 +41,7 @@ def delete_clutter():
                 os.remove(entry)
 
 if __name__ == "__main__":
-    main()
-    delete_clutter()
+    while LOWER_TIME_LIMIT.time() < datetime.datetime.now().time() and datetime.datetime.now().time() < UPPER_TIME_LIMIT.time():
+        main()
+        delete_clutter()
+        time.sleep(60)
